12f6adedc10b5dc7c3d30d33dfbd28fd85a080e1
A01749448 <a01749448@itesm.mx>
2021-04-12 22:28:37 -0500

    Carpetas y Formulario

---
+++ public/js/jsFormulario.js
@@ -0,0 +1,9 @@
+function validar(){
+    var generoJugador= document.getElementById('genero')
+    if (document.generoJugador.value == 0)
+        alert('Completa este campo')
+        document.generoJugador.focus()
+        return 0;
+    }
+};
+
