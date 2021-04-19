158d0d39b19362f20a9f8984781ad77a107d699c
Liam Garay <65176402+A01750632@users.noreply.github.com>
2021-04-19 13:06:31 -0500

    revisión de archivos

--- controllers/menu.js
+++
@@ -1,8 +0,0 @@
-// Modelo asociado a la tabla jugador 
-const path = require('path');
-const sequelize = require('../util/database');
-
-
-exports.getPaginaPrincipal = (req, res)=>{
-    res.sendFile(path.join(__dirname, '..', 'views', 'principal.html')); 
-};  
