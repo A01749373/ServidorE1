9463bec20e33e0591c3cb54f4f6eae37cf6fb979
A01749448 <a01749448@itesm.mx>
2021-04-22 23:41:31 -0500

    Modificaciones controller preguntaContestada

--- controllers/preguntaContestada.js
+++ controllers/preguntaContestada.js
@@ -1,7 +1,7 @@
 //Traer el modelo dentro del objeto de conexión
 const PreguntaContestada = require('../util/database').models.preguntaContestada;
 
-exports.postAgregarPreguntaContestada =(req,res)=>{
+/*exports.postAgregarPreguntaContestada =(req,res)=>{
     console.log(req.body);
     PreguntaContestada.create(req.body)
         .then(resultado=>{
@@ -12,4 +12,17 @@ exports.postAgregarPreguntaContestada =(req,res)=>{
             console.log(error);
             res.send("Hubo un problema");
         })
-}
+}*/
+
+exports.postAgregarPreguntaContestada = (req,res)=>{
+    console.log(req.body);
+    PreguntaContestada.create({
+        opcionContestada: req.body.opcionContestada,
+        estado: req.body.estado,
+        //username: req.body.Username,
+        //preguntaIdPregunta: idPregunta
+    }).then(resultado=>console.log("Registro exitoso"))
+    .catch(error=>console.log(error)); 
+
+    res.send("")
+ }; 
