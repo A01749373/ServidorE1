253ad64e66827c823f07aa2033b67f4be201072b
a01750147 <A01750147@itesm.mx>
2021-04-27 14:20:44 -0500

    BuscarNivelDesbloqueado

--- controllers/partida.js
+++ controllers/partida.js
@@ -22,3 +22,41 @@ exports.postAgregarPartida = (req,res)=>{
       })
       res.send('Partida Agregada')
 };
+
+exports.postBuscarJugador= (req,res)=>{
+    console.log(req.body)
+    Jugador.findAll({
+    where: {
+        username: req.body.usuarioUsuarioo,
+        password: req.body.passwordUsuarioo
+      }
+    })
+    .then(registros=>{
+        //console.log(registros)
+        var data=[];
+        registros.forEach(registro=>{
+            data.push(registro.dataValues);
+        });
+        console.log(data)
+        if (registros.length == 0){
+            res.send('Usuario o contraseña incorrecto')
+        }else{
+            res.send('')
+        }
+    }).catch(error => console.log(error))
+};
+
+exports.postBuscarNivel= (req,res)=>{
+    //console.log(req.body)
+    sequelize.query("select max([nivelIdNivel]) as nivel from [dbo].[partida] where [jugadorUsername] =" + "'" + req.body.usuarioUsuarioo +"'",{
+        type: Sequelize.QueryTypes.SELECT
+    })
+    .then(registros=>{
+        var data = [];
+        registros.forEach(registro=>{
+            data.push(registro);
+        })
+    //console.log((String(data[0].nivel)));
+    res.send(String(data[0].nivel))
+    }).catch(error => console.log(error))
+};
--- routes/partida.js
+++ routes/partida.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const partidaController = require('../controllers/partida');
 
 router.post('/agregarPartida', partidaController.postAgregarPartida);
+router.post('/BuscarNivel', partidaController.postBuscarNivel);
 
 module.exports = router;
